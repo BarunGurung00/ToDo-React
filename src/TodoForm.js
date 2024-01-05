4b13fa81647b79eef7e63e13c9b85ee9c6a5b524
barungurung00@gmail.com <barun>
2024-01-05 18:57:06 +0000

    slight change to TodoForm.js

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import ToDos from "./Todotask";
 import { MdOutlinePlaylistAdd } from "react-icons/md";
+import ToDos from "./Todotask";
 
 export default function TodoForm() {
   const [input, setInput] = useState("");
@@ -14,7 +14,7 @@ export default function TodoForm() {
 
   const addTask = (e) => {
     e.preventDefault();
-    if(input === "" || input === " ")return;
+    if (input === "" || input === " ") return;
 
     setTask([
       ...task,
@@ -42,8 +42,10 @@ export default function TodoForm() {
   return (
     <form className="form w-96 mt-16">
       <div className="top-bar">
-        <h1 className="text-center mb-4 font-bold text-lg h-full">To Do Lists</h1>
-        <div className="h-8 w-auto flex gap-4 items-center " >
+        <h1 className="text-center mb-4 font-bold text-lg h-full">
+          To Do Lists
+        </h1>
+        <div className="h-8 w-auto flex gap-4 items-center ">
           <input
             className="rounded-[2px] border-[0.5px] border-gray-600"
             type="text"
@@ -51,7 +53,12 @@ export default function TodoForm() {
             onChange={handleChange}
             placeholder="Enter todo task here"
           />
-          <MdOutlinePlaylistAdd size="30" className="rounded-sm border-[1px] border-gray-400" type="submit" onClick={addTask}/>
+          <MdOutlinePlaylistAdd
+            size="30"
+            className="rounded-sm border-[1px] border-gray-400"
+            type="submit"
+            onClick={addTask}
+          />
         </div>
       </div>
       <ToDos className="mt-2" tasks={task} onDeleteTask={deleteTask} />
